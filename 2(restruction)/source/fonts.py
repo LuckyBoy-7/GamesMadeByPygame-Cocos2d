5aa9f22a7aca8bb221901072875c03d75a132ad5
look_for_happiness <103829994+userzbh666@users.noreply.github.com>
2023-03-01 22:31:47 +0800

    Delete 2(restruction) directory

--- 2(restruction)/source/fonts.py
+++
@@ -1,7 +0,0 @@
-from pygame import font
-
-
-class Fonts(object):
-    @classmethod
-    def my_font(cls, font_size):
-        return font.Font("./resources/fonts/Determination.otf", font_size)
--- 2(restruction)/source/stats.py
+++
@@ -1,6 +0,0 @@
-class Stats(object):
-    def __init__(self):
-        self.harvest = 0
-
-
-stats = Stats()
