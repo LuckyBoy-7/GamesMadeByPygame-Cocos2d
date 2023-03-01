317b96ae3aa82cee2ea54c6de77def87e60bd294
look_for_happiness <103829994+userzbh666@users.noreply.github.com>
2023-03-01 22:53:38 +0800

    Add files via upload

---
+++ Others/PlaneWars/source/background.py
@@ -0,0 +1,23 @@
+from image import Image
+
+
+class Background:
+    def __init__(self):
+        self.image = Image.background
+        self.rect = self.image.get_rect()
+        self.y = float(self.rect.y)
+
+    def update(self):
+        # move down background image
+        if self.y < self.rect.height:
+            self.y += 7
+        else:
+            self.y = 0
+            # self.y -= self.rect.height  # 我觉得这样更好?
+
+        self.rect.y = self.y
+
+    def draw(self, display_surface):
+        # draw 2 images onto display surface
+        display_surface.blit(self.image, (0, self.rect.y))
+        display_surface.blit(self.image, (0, self.rect.y - self.rect.height))
